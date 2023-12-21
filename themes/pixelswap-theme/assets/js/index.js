9b8d96dbb2effae401455c977f49c8e9459f52f5
Corentin Mors <corentin@dashlane.com>
2023-12-21 18:02:57 +0100

    Add dark / light mode switcher

--- themes/pixelswap-theme/assets/js/index.js
+++ themes/pixelswap-theme/assets/js/index.js
@@ -1,1 +1,22 @@
 hljs.initHighlightingOnLoad();
+
+const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (localStorage.getItem('dark-mode') === null) {
+    localStorage.setItem('dark-mode', true);
+}
+
+if (prefersDark || localStorage.getItem('dark-mode') === 'true') {
+    document.documentElement.classList.add('dark');
+}
+
+document.querySelectorAll('.dark-mode-switcher').forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        document.documentElement.classList.toggle('dark');
+        element.firstChild.classList.toggle('fa-moon');
+        element.firstChild.classList.toggle('fa-sun');
+        localStorage.setItem('dark-mode', document.documentElement.classList.contains('dark'));
+    });
+});
