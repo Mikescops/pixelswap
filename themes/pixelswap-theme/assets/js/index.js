fc08a899105171c12f451a4a2bef616d6311f856
Corentin Mors <corentin@dashlane.com>
2023-12-23 12:46:49 +0100

    Better dark mode, pure css

--- themes/pixelswap-theme/assets/js/index.js
+++ themes/pixelswap-theme/assets/js/index.js
@@ -1,12 +1,1 @@
 hljs.initHighlightingOnLoad();
-
-document.querySelectorAll('.dark-mode-switcher').forEach((element) => {
-    element.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        document.documentElement.classList.toggle('dark');
-        element.firstChild.classList.toggle('fa-moon');
-        element.firstChild.classList.toggle('fa-sun');
-        localStorage.setItem('dark-mode', document.documentElement.classList.contains('dark'));
-    });
-});
